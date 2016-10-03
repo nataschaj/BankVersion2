f777417fb42682f1943ac19ab49dcab67d4f0290
EASJ <EASJ@EASJ3212>
2016-10-03 17:20:38 +0200

    Add project files.

---
+++ Sandbox/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    public class BankAccount
+    {
+        private double balance;
+
+        public BankAccount()
+        {
+            balance = 0;
+        }
+
+        public void Deposit(double amount)
+        {
+            balance = balance + amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            balance = balance - amount;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ Sandbox/InsertCodeHere.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sandbox
+{
+    class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            BankAccount myAccount = new BankAccount();
+
+            // Test the BankAccount - make some deposits and withdrawals,
+            // and check that the balance behaves as expected
+            myAccount.GetBalance();
+            Console.WriteLine("Balance is now : {0}", myAccount.GetBalance());
+
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
