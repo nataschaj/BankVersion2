6bdc7b33ddb3ccd8a1a8ac517f852e9ecd67b5dd
Natascha Jakobsen <natascha_jakobsen@hotmail.com>
2016-10-23 22:46:20 +0200

    nothing

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -15,6 +15,11 @@ namespace Sandbox
             myAccount.GetBalance();
             Console.WriteLine("Balance is now : {0}", myAccount.GetBalance());
 
+            myAccount.Withdraw(-1000);
+            Console.WriteLine("Balance is now : {0}", myAccount.GetBalance());
+
+            myAccount.Deposit(-2000);
+            Console.WriteLine("Balance is now : {0}", myAccount.GetBalance());
 
             // The LAST line of code should be ABOVE this line
         }
